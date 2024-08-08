1d97a9338f3c2ed9abad0ab258815262089198c1
jjmchew <jjmchew@gmail.com>
2024-08-08 14:55:01 -0600

    feat: add cdk script for ec2 instance and updated setup script

--- setup_scripts/setup_ec2.sh
+++ setup_scripts/setup_ec2.sh
@@ -11,22 +11,23 @@ echo "========= install nginx"
 
 sudo apt -y install nginx
 
-
 echo ""
 echo "========= installing pyenv"
 
 curl https://pyenv.run | bash
 
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
-echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile
-echo 'eval "$(pyenv init -)"' >> ~/.profile
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >>~/.profile
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >>~/.profile
+echo 'eval "$(pyenv init -)"' >>~/.profile
+
+source ~/.profile
 
 echo ""
 echo "========= installing build dependencies for python"
 
 sudo apt -y install build-essential libssl-dev zlib1g-dev \
-libbz2-dev libreadline-dev libsqlite3-dev curl \
-libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+	libbz2-dev libreadline-dev libsqlite3-dev curl \
+	libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 
 echo ""
 echo "========= installing python 3.10.12"
@@ -38,8 +39,3 @@ echo ""
 echo "========= installing pipenv"
 
 pip install pipenv
-
-source ~/.profile
-
-
-
